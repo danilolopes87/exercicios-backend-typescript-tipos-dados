88f5c13cb72f368e66ca844d67ad105ceff87368
87ehnosso <114113142+danilolopes87@users.noreply.github.com>
2024-02-29 17:53:22 -0300

    ok

---
+++ 04/script.ts
@@ -0,0 +1,17 @@
+const imprimirEtiquetas = (info: {produto: string, lote: number, ano: number, qtd: number}): string[] => {
+    const resultado: string[] = [];
+    for (let i = 1; i <= info.qtd; i++) {
+        resultado.push(`${info.lote}-${info.ano}-${i.toString().padStart(3, '0')}`)
+    }
+
+    return resultado;
+}
+
+const produto =   {
+    produto: 'CPU Dual Core 3.0GHZ',
+    lote: 321,
+    ano: 2022,
+    qtd: 5
+}
+
+console.log(imprimirEtiquetas(produto));
